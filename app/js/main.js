6a3effb258e2836a5c49653ca223a636c1e58690
Alexander Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2019-01-14 21:56:20 +0200

    Create the menu navigation

--- app/js/main.js
+++ app/js/main.js
@@ -5,10 +5,23 @@
     $(window).scroll(() => {
         var windowTop = $(window).scrollTop();
         windowTop > 100 ? $('.nav').css('height', '60px') : $('.nav').css('height', '100px');
+        windowTop > 100 ? $('.nav__mob').css('top', '60px') : $('.nav__mob').css('top', '100px');
+        windowTop > 100 ? $('.logo').css('display', 'none') : $('.logo').css('display', 'block');
     });
 
+    //Mobile menu class toggle
     $('.nav__icon').click(function(){
         $(this).toggleClass('open');
+        $('.nav__mob').toggleClass('nav__mob--open');
+    });
+
+    $(window).on('resize', function() {
+        var win = $(this);
+        if (win.width() > 650) {
+            if ($(".nav__mob").hasClass("nav__mob--open")) {
+                $(".nav__mob").removeClass("nav__mob--open");
+            }
+        }
     });
 
 })(jQuery);
