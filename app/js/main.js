5c13eaf2fd62d4c9d186ca5b156c54440f61a038
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2019-01-14 17:33:38 +0200

    Creating navigation

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 ;(function($){
     "use strict";
 
+    //On Scroll Functionality
+    $(window).scroll(() => {
+        var windowTop = $(window).scrollTop();
+        windowTop > 100 ? $('.nav').css('height', '60px') : $('.nav').css('height', '100px');
+    });
+
+    $('.nav__icon').click(function(){
+        $(this).toggleClass('open');
+    });
+
 })(jQuery);
